38787ea13c6aa1460307ef94622a1240bcc5cf22
Shail <shailpatel2@gmail.com>
2015-12-28 22:45:37 -0800

    Mostly finish up data collection, will port to using API instead of raw mysql

--- scripts/data_collection.rb
+++ scripts/data_collection.rb
@@ -14,6 +14,14 @@ def collect_table_data(url)
   page = @mechanize.get(url)
 
   table = page.search("//table[@class='table table-striped table-bordered']").first.children
+
+  if url.include?("supercontest-selection")
+    week = page.search("//h1").first.children.first.text.match(/\d{1,}$/)[0].to_i
+  else
+    week = (table[3].children[6].children.text.to_i + table[3].children[7].children.text.to_i +
+    table[3].children[8].children.text.to_i) / 5
+  end
+
   picks_array = []
   table.each do |element|
     temp_array = []
@@ -24,29 +32,42 @@ def collect_table_data(url)
       picks_array << temp_array
     end
   end
-  picks_array
+
+  [picks_array, week]
 end
 
 standings_data = collect_table_data("https://www.westgatedestinations.com/nevada/las-vegas/westgate-las-vegas-hotel-casino/casino/supercontest-standings")
-standings_data.shift
-standings_data.each do |standing|
-  # Create the team if it hasn't been created already
-  team = client.query("SELECT * FROM teams WHERE name='#{standing.first}'")
+standings_data.first.shift
+puts "Inserting standings data"
+standings_data.first.each do |standing|
+  team = client.query("SELECT * FROM teams WHERE name=\"#{standing.first}\"")
+
   if team.first.nil?
-    client.query("INSERT INTO teams (name) values ('#{standing.first}')")
+    client.query("INSERT INTO teams (name) values (\"#{standing.first}\")")
     team_id = client.last_id
   else
-    team_id = team.first.id
+    team_id = team.first["id"]
   end
-  pts = standing[1] + 0.5 * standing[3]
-  client.query("INSERT INTO contest_weekly_results (week, team_id, wins, loss, ties, pts)
-  # Get the id of the team and insert into weekly results and then total standings, in this case week =
-  # actual_week - 1
+
+  weekly_pts = standing[1].to_i + 0.5 * standing[3].to_i
+  total_pts = standing[8].to_i + 0.5 * standing[7].to_i
+  client.query("INSERT INTO team_standings (team_id, win, loss, tie, pts) values (#{team_id}, #{standing[8].to_i}, #{standing[6].to_i}, #{standing[7].to_i}, #{total_pts})")
+
+  client.query("INSERT INTO contest_weekly_results (week, team_id, win, loss, tie, pts) values (#{standings_data[1]}, #{team_id}, #{standing[1].to_i}, #{standing[2].to_i}, #{standing[3].to_i}, #{weekly_pts})")
 end
 
+puts "Insert picks data"
 picks_data = collect_table_data("https://www.westgatedestinations.com/nevada/las-vegas/westgate-las-vegas-hotel-casino/casino/supercontest-selections")
-picks_data = picks_data.select { |picks| picks.length == 6 }
-picks_data.shift
-picks_data.each do |pick|
-  # Get the id of the team and then insert the picks into the weekly picks table
+picks_data[0] = picks_data.first.select { |picks| picks.length == 6 }
+picks_data.first.shift
+
+picks_data.first.each do |picks|
+  puts "SELECT * from teams where name=\"#{picks.first}\"".inspect
+  team_id = client.query("SELECT * from teams where name=\"#{picks.first}\"").first["id"]
+
+  picks.shift
+  picks.each do |pick|
+puts "INSERT INTO contest_weekly_picks (team_id, week, team) values (#{team_id}, #{picks_data[1]}, #{pick})".inspect
+    client.query("INSERT INTO contest_weekly_picks (team_id, week, team) values (#{team_id}, #{picks_data[1]}, #{pick})")
+  end
 end
