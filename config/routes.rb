faa31b3d190cddba74aff841b599bbccc016756f
Shail <shailpatel2@gmail.com>
2016-01-04 21:33:43 -0800

    Add in correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :teams
   resources :team_standings
   resources :contest_weekly_picks
-  resources :weekly_picks
+  resources :weekly_results
+  resources :contest_weekly_results
 end
